50aa9bb7df6760f293df20391c75dbebc7e30bbb
emekarr <aceanyanwu02@gmail.com>
2024-08-14 14:38:34 +0100

    fix : prevented chracters other than alphabets from being validated as alphabets

--- src/rules/lowerCaseRule.ts
+++ src/rules/lowerCaseRule.ts
@@ -1,10 +1,12 @@
 import { RuleOptions, ValidationRule } from "../types";
+import { isAlphabet } from "../utils";
 
 const lowerCaseRule: ValidationRule<number> =
   (opts: RuleOptions<number>) => (password: string) => {
     if (opts.value <= 0) return;
     let upperCaseCount = 0;
     for (const char of password) {
+      if (!isAlphabet(char)) continue;
       if (char === char.toLowerCase()) upperCaseCount++;
       if (upperCaseCount === opts.value) return;
     }
--- src/rules/upperCaseRule.ts
+++ src/rules/upperCaseRule.ts
@@ -1,10 +1,12 @@
 import { RuleOptions, ValidationRule } from "../types";
+import { isAlphabet } from "../utils";
 
 const upperCaseRule: ValidationRule<number> =
   (opts: RuleOptions<number>) => (password: string) => {
     if (opts.value <= 0) return;
     let upperCaseCount = 0;
     for (const char of password) {
+      if (!isAlphabet(char)) continue;
       if (char === char.toUpperCase()) upperCaseCount++;
       if (upperCaseCount === opts.value) return;
     }

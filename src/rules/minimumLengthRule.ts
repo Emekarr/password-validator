256666c3e5bdbdec56a16b835f8d280a201de9fe
emekarr <aceanyanwu02@gmail.com>
2024-08-14 13:32:34 +0100

    feat : prevent users from setting maximum or minimum value rules to 0 or less

--- src/rules/minimumLengthRule.ts
+++ src/rules/minimumLengthRule.ts
@@ -2,6 +2,8 @@ import { RuleOptions, ValidationRule } from "../types";
 
 const minLengthRule: ValidationRule =
   (opts: RuleOptions<number>) => (password: string) => {
+    if (opts.value <= 0)
+      throw new Error("minimum length cannot be equal to or less than 0");
     if (password.length < opts.value)
       throw new Error(
         opts.customErrMsg
